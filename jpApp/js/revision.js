30be570815e83d33b17957e97367d1357414d8d3
Tan Wei Shen <tanweishen97@gmail.com>
2019-04-15 23:22:50 +0800

    Deleted unnecessary files

--- jpApp/js/revision.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-
-function checkInput(jpWord) {
-    userMeaning = document.getElementById("meaning");
-    userPrc = document.getElementById("pronunciation")
-
-    console.log(jpWord)
-}
